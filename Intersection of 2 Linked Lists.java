6a0571cc8c8dccda1b53b6613c3b54e0c587b4a2
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2022-10-10 18:10:45 +0200

    intersection of 2 linked lists

---
+++ Intersection of 2 Linked Lists.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode node1 = headA, node2 = headB;
+        while(node1 != node2){
+            node1 = node1 == null ? headB : node1.next;
+            node2 = node2 == null ? headA : node2.next;
+        }
+        return node1;
+    }
+}
