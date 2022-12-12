8cbcf8bbfb8ad0e91a494ad6841197856b590859
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2022-12-13 00:19:19 +0200

    is subsequence

---
+++ is Subsequence.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length(), m = t.length(), j=0;
+        for(int i=0; i<m && j<n; i++)
+        if (s.charAt(j) == t.charAt(i)) j++;
+        return j==n;
+
+    }
+}
